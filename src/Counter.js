c5fe335738d87930a6acdce0ce75d4f314dc5f85
Garikipati SudhakaraRao <garikipati.sudhakarrao@gmail.com>
2026-04-20 09:30:36 +0530

    Start periodic counter on button click event

--- src/Counter.js
+++ src/Counter.js
@@ -1,33 +1,41 @@
 import React, { useEffect, useState } from "react";
 
 const Counter = (props) => {
-  const start = parseInt(props.init || 0);
+  const init = parseInt(props.init || 0);
   const end = parseInt(props.end || 0);
-  const [count, setCount] = useState(start);
+  const [count, setCount] = useState(init);
+  const [start, setStart] = useState(false);
+
+  function startIteration(){
+    setStart(!start);
+  }
 
   useEffect(() => {
-    if(count >= end)
-        return;
-    var timer = setInterval(()=>{
+    if (start) {
+      if (count >= end) return;
+      var timer = setInterval(() => {
         setCount(count + 1);
-    }, 1000);
-    return () => {
+      }, 1000);
+      return () => {
         clearInterval(timer);
+      };
     }
   });
 
   return (
     <div>
-      Initial value of the counter is: {start}
-      <br/>
+      Initial value of the counter is: {init}
+      <br />
       End of the counter at: {end}
-      <br/>
+      <br />
       The counter is: {count}
-      <br/>
-      {
-        (end && count >= end) ? <b>Counter stopped.</b>:<i>Counter in progress.</i>
-      }
-      <button>Click Me</button>
+      <br />
+      {end && count >= end ? (
+        <b>Counter stopped.</b>
+      ) : (
+        <i>Counter in progress.</i>
+      )}
+      <button onClick={startIteration}>Click Me</button>
     </div>
   );
 };
